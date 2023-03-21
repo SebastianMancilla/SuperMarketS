4746155c1dafab3af35e9373b568d3cd6bd6ce84
SebastianMancilla <gonzalezsebas43@gmail.com>
2023-03-20 22:36:54 -0600

    Modify ProductDTO

--- src/main/java/com/jsmg/sumermarkets/dto/ProductDTO.java
+++ src/main/java/com/jsmg/sumermarkets/dto/ProductDTO.java
@@ -1,6 +1,5 @@
 package com.jsmg.sumermarkets.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jsmg.sumermarkets.domain.Supplier;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
@@ -20,11 +19,4 @@ public class ProductDTO {
     private BigDecimal price;
     private Integer existence;
     private Supplier supplierId;
-
-    @JsonProperty("supplierId")
-    private void unpackNested(Long id){
-        this.supplierId = new Supplier();
-        supplierId.setId(id);
-    }
-
 }
